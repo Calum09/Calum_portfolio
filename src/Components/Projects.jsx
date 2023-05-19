c587745c1620bdf6d379d8d616f713dd36ad7722
Charlotte Pearce <charlottepearce@charlottes-mbp.home>
2023-05-19 15:03:10 +0100

    made changes to image folder structure

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -12,7 +12,7 @@ export default function Projects() {
     <div className="portfolio">
         <div className="portfolio__item">
         <a href="https://calum09.github.io/Film-Finder/" >
-            <img src="Images/Film Guide.png" alt="Film Finder" className="portfolio__img"/>
+            <img src="/Images/Film Guide.png" alt="Film Finder" className="portfolio__img"/>
         </a>
         <h3>Film Finder</h3>
         <p className="portfolio-description"><strong>JavaScript, HTML & CSS</strong> - Film Finder is a web app that uses the OMDB api to display information about the film you have searched.</p>
@@ -20,7 +20,7 @@ export default function Projects() {
 
         <div className="portfolio__item">
         <a href="https://calum09.github.io/WeatherApp/">
-            <img src="Images/weather-app.png" alt="Weather App" className="portfolio__img"/>
+            <img src="/Images/weather-app.png" alt="Weather App" className="portfolio__img"/>
         </a>
         <h3>Weather App</h3>
         <p className="portfolio-description"><strong>JavaScript, HTML & CSS</strong> - A weather app created using the Open Weather API to display the current weather in a given city.</p>
@@ -28,7 +28,7 @@ export default function Projects() {
 
         <div className="portfolio__item">
         <a href="https://calum09.github.io/Login-Page/">
-            <img src="Images/login-page.png" alt="Login Page" className="portfolio__img"/>
+            <img src="/Images/login-page.png" alt="Login Page" className="portfolio__img"/>
         </a>
         <h3>Login Page</h3>
         <p className="portfolio-description"><strong>HTML & CSS</strong> - A login landing page.</p>
