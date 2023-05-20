99609e9f24c1d0682d947189430725d1b72a338b
Charlotte Pearce <charlottepearce@charlottes-mbp.home>
2023-05-20 17:28:27 +0100

    minor changes

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -17,7 +17,7 @@
             </p>
             <p>BA in Computing and History from the University of Kent.</p>
 
-            <h2>Tools & Tech</h2>
+            <h2 className="section__title section__title--about">Tools & Tech</h2>
             <p>JavaScript | Python | HTML | CSS | React.js | Redux | Node.js | Express.js | PostgreSQL | Mocha & Chai | Jest |   
                Git & Version Control</p>
         </div>
