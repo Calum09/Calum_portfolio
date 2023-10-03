0533dc9e3ae4833f16b36438a83b7174f0239e8f
Calum Laverick <calumlaverick09@gmail.com>
2023-10-03 20:47:34 +0100

    changed the about me

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -11,14 +11,13 @@
         <p className="section__subtitle section__subtitle--about">Software Engineer based out of London</p>
         
         <div className="about-me__body">
-            <p>Software Engineer with 4 years of experience in the games industry. With a background in QA testing and digital operations.
-            I've worked at top companies including Omeda Studios and Creative Assembly, 
-            contributing to the development of the Total War series and Predecessor.
+            <p>Software Engineer currently proficient in Frontend and Backend, currently working with TypeScript, HTML, TailwindCSS React.js and Next.js to create a one stop site for all your AI needs
+               You can check out the progress in my github reprository - https://github.com/Calum09/AI-Expert
             </p>
             <p>BA in Computing and History from the University of Kent.</p>
 
             <h2 className="section__title section__title--about">Tools & Tech</h2>
-            <p> JavaScript/TypeScript | Python | HTML | CSS | React.js | Redux | Node.js | Express.js | PostgreSQL | Mocha & Chai | Jest |   
+            <p> JavaScript/TypeScript | Python | HTML | CSS | TailwindCSS | React.js | Next.js | Redux | Node.js | Express.js | PostgreSQL | Mocha & Chai | Jest |   
                Git & Version Control</p>
         </div>
      
