f6e9e5df95690dd223cc1222b59f4aca29169576
Calum Laverick <calumlaverick09@gmail.com>
2023-10-03 20:57:36 +0100

    changed the about me

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -11,13 +11,13 @@
         <p className="section__subtitle section__subtitle--about">Software Engineer based out of London</p>
         
         <div className="about-me__body">
-            <p>Software Engineer currently proficient in Frontend and Backend, currently working with TypeScript, HTML, TailwindCSS React.js and Next.js to create a one stop site for all your AI needs
-               You can check out the progress in my github reprository - https://github.com/Calum09/AI-Expert
+            <p>Software Engineer proficient in frontend and backend, currently working with TypeScript, HTML, TailwindCSS, React.js, Next.js, Node.js, Shadcn UI and Git to create a one stop site for all your AI needs
+               You can check out the progress in my github reprository - <a href="https://github.com/Calum09/AI-Expert">AI Expert</a>
             </p>
             <p>BA in Computing and History from the University of Kent.</p>
 
             <h2 className="section__title section__title--about">Tools & Tech</h2>
-            <p> JavaScript/TypeScript | Python | HTML | CSS | TailwindCSS | React.js | Next.js | Redux | Node.js | Express.js | PostgreSQL | Mocha & Chai | Jest |   
+            <p> JavaScript/TypeScript | Python | HTML | CSS | TailwindCSS | React.js | Next.js | Redux | Node.js | Express.js | PostgreSQL | Mocha & Chai | Jest |  
                Git & Version Control</p>
         </div>
      
