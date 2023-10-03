b9a332389bf9592c7009966e0b783d3d0844d2cf
Calum Laverick <calumlaverick09@gmail.com>
2023-10-03 21:01:20 +0100

    edit about me

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -12,7 +12,7 @@
         
         <div className="about-me__body">
             <p>Software Engineer proficient in frontend and backend, currently working with TypeScript, HTML, TailwindCSS, React.js, Next.js, Node.js, Shadcn UI and Git to create a one stop site for all your AI needs
-               You can check out the progress in my github reprository - <a href="https://github.com/Calum09/AI-Expert">AI Expert</a>
+               You can check out the progress in my github reprository - <a className="social-list__link" href="https://github.com/Calum09/AI-Expert">AI Expert</a>
             </p>
             <p>BA in Computing and History from the University of Kent.</p>
 
